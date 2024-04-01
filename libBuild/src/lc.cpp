8a3bde84569dee9e99d60f2565df7b8f8435b054
RealFaceCode <kevin-ernst@xyz.de>
2024-04-01 21:11:16 +0200

    Add log type order for all log types

--- libBuild/src/lc.cpp
+++ libBuild/src/lc.cpp
@@ -10,5 +10,20 @@ namespace lc
         AddLogType("WARNING");
         AddLogType("ERROR");
         AddLogType("CRITICAL");
+
+        std::initializer_list<LogOrder> logOrder = 
+        {
+            LogOrder::Level,
+            LogOrder::Time,
+            LogOrder::Date,
+            LogOrder::Function, 
+            LogOrder::Line, 
+            LogOrder::Message
+        };
+        SetLogTypeOrder("INFO", logOrder);
+        SetLogTypeOrder("DEBUG", logOrder);
+        SetLogTypeOrder("WARNING", logOrder);
+        SetLogTypeOrder("ERROR", logOrder);
+        SetLogTypeOrder("CRITICAL", logOrder);
     }
 }
