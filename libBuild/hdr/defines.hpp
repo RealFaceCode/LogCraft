17c103d82d27b94f9316fc73266d8c0b5e427a5c
RealFaceCode <kevin-ernst@xyz.de>
2024-04-02 22:45:38 +0200

    Add platform-specific DLL export/import macros

--- libBuild/hdr/defines.hpp
+++ libBuild/hdr/defines.hpp
@@ -4,8 +4,16 @@
 #define LC_VERSION_MINOR 1
 #define LC_VERSION_PATCH 0
 
-#ifdef LOGCRAFT_EXPORTS
-    #define LOGCRAFT_API __declspec(dllexport)
+#ifdef _WIN32
+        #define DLL_EXPORT __declspec(dllexport)
+        #define DLL_IMPORT __declspec(dllimport)
 #else
-    #define LOGCRAFT_API __declspec(dllimport)
+        #define DLL_EXPORT __attribute__((visibility("default")))
+        #define DLL_IMPORT __attribute__((visibility("default")))
+#endif
+
+#ifdef LOGCRAFT_EXPORTS
+    #define LOGCRAFT_API DLL_EXPORT
+#else
+    #define LOGCRAFT_API DLL_IMPORT
 #endif
